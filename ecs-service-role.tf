a5f625d74be78592a7c0f6d04259b7a059e5bd27
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2018-11-19 18:15:32 +0200

    Add ECR pull rights

--- ecs-service-role.tf
+++ ecs-service-role.tf
@@ -9,13 +9,19 @@ resource "aws_iam_role_policy_attachment" "ecs-service-role-attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 
+resource "aws_iam_role_policy_attachment" "ecs-service-role-attachment2" {
+  role       = "${aws_iam_role.ecs-service-role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
 data "aws_iam_policy_document" "ecs-service-policy" {
   statement {
     actions = ["sts:AssumeRole"]
 
     principals {
       type        = "Service"
-      identifiers = ["ecs.amazonaws.com"]
+      identifiers = ["ecs.amazonaws.com","ecs-tasks.amazonaws.com"]
     }
   }
 }
